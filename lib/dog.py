d3bf2dbe12b0d8f62fa8a9f79171da2bf2e84544
Mathew-Rym <rymmathew@gmail.com>
2025-05-19 00:01:19 +0300

    passed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name
+        self.breed = breed
+    
+    def print_self(self):
+        print(f"Dog name is {self.name}")
+        print(f"Dog breed is {self.breed}")
+
+bosco = Dog("Bosco", "universal")
+
+bosco.print_self()
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+        
+    def print_details(self):
+        print("My name is", self.name)
+        
+
+person1 = Person("Boru")
+person1.print_details()
